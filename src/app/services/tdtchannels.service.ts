8d74b579396a303cd5dba2974044de0dc938e8c3
joanlloria <joan.lloria@cabsa.es>
2026-02-09 10:55:14 +0100

    Use Cloudflare proxy for CORS

--- src/app/services/tdtchannels.service.ts
+++ src/app/services/tdtchannels.service.ts
@@ -10,12 +10,14 @@ export class TdtchannelsService {
   TDT_JSON_URL_TV = 'https://www.tdtchannels.com/lists/tv.json';
   TDT_JSON_URL_RADIO = 'https://www.tdtchannels.com/lists/radio.json';
 
+  CLOUDFLARE_WORKER_CORS_URL = 'https://fragrant-shape-d81c.joanlloria.workers.dev';
+
   constructor(
     private readonly http: HttpClient
   ) { }
 
   getTvChannels(): Observable<TdtChannelsResponse> {
-    return this.http.get<TdtChannelsResponse>(this.TDT_JSON_URL_TV).pipe(
+    return this.http.get<TdtChannelsResponse>(this.CLOUDFLARE_WORKER_CORS_URL+'?url='+encodeURIComponent(this.TDT_JSON_URL_TV)).pipe(
       catchError(err => {
         const fallbackUrl = 'data/tv.json';
         console.warn(`API failed (CORS?), loading local JSON tv instead from "${fallbackUrl}"`, err);
@@ -25,9 +27,9 @@ export class TdtchannelsService {
   }
 
   getRadioStations(): Observable<TdtChannelsResponse> {
-    return this.http.get<TdtChannelsResponse>(this.TDT_JSON_URL_RADIO).pipe(
+    return this.http.get<TdtChannelsResponse>(this.CLOUDFLARE_WORKER_CORS_URL+'?url='+encodeURIComponent(this.TDT_JSON_URL_RADIO)).pipe(
       catchError(err => {
-        const fallbackUrl = 'data/radio.json';
+        const fallbackUrl = 'data/tradio.json';
         console.warn(`API failed (CORS?), loading local JSON radio instead from "${fallbackUrl}"`, err);
         return this.http.get<TdtChannelsResponse>(fallbackUrl);
       })
@@ -35,7 +37,7 @@ export class TdtchannelsService {
   }
 
   getEpg(url: string): Observable<TdtEpgItem[]> {
-    return this.http.get<TdtEpgItem[]>(url);
+    return this.http.get<TdtEpgItem[]>(this.CLOUDFLARE_WORKER_CORS_URL+'?url='+encodeURIComponent(url));
     /*
     return of ([
       { 
